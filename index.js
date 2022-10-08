2a39872c5941abcb5c01bfa88f34396571efe3a8
Andrew <afellman84@gmail.com>
2022-10-08 16:11:44 -0400

    commit tree c0fd4c52caa98c5e2089779dd3316f834ff061eb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let text = document.getElementById("text");
+    console.log("text", text)
+    text.textContent = "This is really cool!"
+});
+
+
+const event = document.createEvent("event")
+event.initEvent("DOMContentLoaded", true, true)
+window.document(dispatchEvent)(event);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("This is really cool!");
+});
